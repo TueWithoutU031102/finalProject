fa476ba1dac849f04046abe380be4f85c5bfc09e
Ku Tue <Ku Tue@DESKTOP-2FB50U9>
2023-05-09 01:50:18 +0700

    làm được 1 tí front-end phần form booking

--- resources/views/Customer/book.blade.php
+++ resources/views/Customer/book.blade.php
@@ -11,12 +11,21 @@
 <body>
     <form action="">
         <div class="input-box">
-            <label for="name" class="form-label">Tên của bạn:</label>
+            <label for="name" class="form-label">Your name:</label>
             <input type="text" class="form-control" id="name" name="name">
         </div>
         <div class="input-box">
             <label for="phonenumber" class="form-label">Phonenumber:</label>
             <input type="text" class="form-control" id="phonenumber" name="phonenumber">
+        </div>
+        <div class="input-box">
+            <label for="numberofPeople" class="form-label">Number of people:</label>
+            <input type="text" class="form-control" id="numberofPeople" name="numberofPeople">
+        </div>
+        <div class="input-box">
+            <label for="arrivalTime" class="form-label">Arrival time:</label>
+            <input type="datetime-local" class="form-control" value="{{ old('arrivalTime') }}" id="arrivalTime"
+                name="arrivalTime">
         </div>
         <div class="button-action">
             <button type="submit" class="btn btn-primary">Submit</button>
